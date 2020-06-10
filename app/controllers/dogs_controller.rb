b533a50090422cc4f48900f37ead894855c12fc4
skandow <kando1sr@gmail.com>
2020-06-10 14:44:10 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,19 @@
 class DogsController < ApplicationController
+    def index 
+        @dogs = Dog.all 
+    end 
+
+    def order
+        @dogs = Dog.all.sort_by do |dog|
+            dog.employees.count 
+        end.reverse 
+        
+        render :index
+    end 
+
+    def show 
+        @dog = Dog.find(params[:id])
+        @employees = @dog.employees 
+    end 
+
 end
