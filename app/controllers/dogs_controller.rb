c5a9e109242bb7b1bd208ce673144663370f94ee
emilioquintana90 <emilioquintana90@gmail.com>
2020-06-11 08:36:55 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,17 @@
 class DogsController < ApplicationController
+  before_action :find_dog, only: [:show]
+  
+  def index
+    @sorted_dogs_by_employees = Dog.sort_by_employees
+  end
+
+  def show
+    @owners = @dog.employees
+  end
+
+  private
+
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,7 @@
 class Dog < ApplicationRecord
+  has_many :employees
+
+  def self.sort_by_employees
+    self.all.sort_by { |dog| -dog.employees.count}
+  end
 end
